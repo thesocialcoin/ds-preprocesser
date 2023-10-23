a688602de5034417233ce3330f65a7e1b4e0c844
Anna Senent <annasenent@MacBook-Air-de-Anna.local>
2023-10-23 17:37:09 +0200

    adding docstrings to improve code documentation

--- src/preprocesser/models/_base.py
+++ src/preprocesser/models/_base.py
@@ -1,5 +1,6 @@
 import re
 import string
+from typing import Tuple, List, Dict, Counter, Optional
 from collections import Counter
 
 from preprocesser.data import make_stopwords
@@ -10,8 +11,12 @@ UNICODES = (
 )
 
 
-def removeUnicode(text):
-    """Removes unicode strings like "\u002c" printable such as \t \r and \n"""
+def removeUnicode(text: str) -> str:
+    """Removes unicode strings like "\u002c" printable such as \t \r and \n
+    
+    Args:
+        text (str): The input text
+    """
     text = re.sub(r"(\\u[0-9A-Fa-f]+)", r"", text)
     text = re.sub(
         r"&amp;", "&", text.replace("\t", " ").replace("\r", " ").replace("\n", " ")
@@ -19,8 +24,12 @@ def removeUnicode(text):
     return text
 
 
-def removeUrls(text):
-    """change URL by special token"""
+def removeUrls(text: str)Tuple[str, Dict[str, List[str]]]:
+    """change URL by special token
+    
+    Args:
+        text (str): The input text
+    """
     urls = re.findall(
         (
             "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]"
@@ -35,8 +44,12 @@ def removeUrls(text):
     return text, {"urls": urls}
 
 
-def removeHtMentionsSuccessions(text):
-    """Remove #s and @s that are at the beginning or at the end of a tweet"""
+def removeHtMentionsSuccessions(text: str)Tuple[str, Dict[str, Optional[str]]]:
+    """Remove #s and @s that are at the beginning or at the end of a tweet
+    
+    Args:
+        text (str): The input text
+    """
     ht_mts = {}
 
     ht_mts["begin"] = re.search(r"^(URL\s*)*([@#][\w+" + UNICODES + r"]+\s){1,}", text)
@@ -84,47 +97,70 @@ def tolower(text: str) -> str:
     return text.lower()
 
 
-def replaceAtUser(text):
-    """Replaces "@user" with "atUser" """
+def replaceAtUser(text: str) -> Tuple[str, Dict[str, List[str]]]:
+    """Replaces "@user" with "atUser"
+    
+    Args:
+        text (str): The input text
+    """
     users = re.findall(r"@[\w+" + UNICODES + "]+", text)
     for user in users:
         text = text.replace(user, "atUser")
     return text, {"users": users}
 
 
-def removeHashtagInFrontOfWord(text):
-    """Removes hastag in front of a word"""
+def removeHashtagInFrontOfWord(text: str) -> Tuple[str, Dict[str, List[str]]]:
+    """Removes hastag in front of a word
+    
+    Args:
+        text (str): The input text
+    """
     hts = re.findall(r"#[\w+" + UNICODES + "]+", text)
     for ht in hts:
         text = text.replace(ht, ht[1:])
     return text, {"#s": hts}
 
 
-def replaceMultiExclamationMark(text):
-    """Replaces repetitions of exlamation marks"""
+def replaceMultiExclamationMark(text: str) -> Tuple[str, Dict[str, int]]:
+    """Replaces repetitions of exlamation marks
+    
+    Args:
+        text (str): The input text
+    """
     exclams = re.findall(r"(\!)\1+", text)
     text = re.sub(r"(\!)\1+", r"!", text)
     return text, {"exclams": len(exclams)}
 
 
-def replaceMultiQuestionMark(text):
-    """Replaces repetitions of question marks"""
+def replaceMultiQuestionMark(text: str) -> Tuple[str, Dict[str, int]]:
+    """Replaces repetitions of question marks
+    
+    Args:
+        text (str): The input text
+    """
     questions = re.findall(r"(\?)\1+", text)
     text = re.sub(r"(\?)\1+", r"?", text)
     return text, {"questions": len(questions)}
 
 
-def replaceMultiStopMark(text):
-    """Replaces repetitions of stop marks"""
+def replaceMultiStopMark(text: str) -> Tuple[str, Dict[str, int]]:
+    """Replaces repetitions of stop marks
+    
+    Args:
+        text (str): The input text
+    """
     stops = re.findall(r"(\.)\1+", text)
     text = re.sub(r"(\.)\1+", r".", text)
     return text, {"stops": len(stops)}
 
 
-def replaceElongated(text):
+def replaceElongated(text: str) -> Tuple[str, Dict[str, int]]:
     """
     Replaces an elongated word with its
     basic form if it contains >2 repeated letters
+
+    Args:
+        text (str): The input text
     """
     quantifiers = '{2,}'
     pattern = fr"([a-zA-Z{UNICODES}]+)\1{quantifiers}"
@@ -136,7 +172,18 @@ def replaceElongated(text):
     return text, {"elongateds": len(elongateds)}
 
 
-def removeEmojis(text):
+def removeEmojis(text: str) -> Tuple[str, Dict[str, List[str]]]:
+    """
+    Replaces emojis in the input text with a placeholder
+
+    Args:
+        text (str): The input text
+
+    Returns:
+        tuple: A tuple containing two elements:
+            - str: The modified text with the placeholder EMOJI instead of the emojis.
+            - dict: A dictionary with the key "emojis" mapping to a list of emojis retrieved from the text.
+    """
 
     emoji_pattern = re.compile(
         "["
@@ -160,7 +207,19 @@ def removeEmojis(text):
     return text, {"emojis": emojis}
 
 
-def removeNumbers(text):
+def removeNumbers(text: str) -> Tuple[str, Dict[str, List[str]]]:
+    """
+    Replaces numbers (dates, prices, years and other numbers) in the input text with placeholders.
+
+    Args:
+        text (str): The input text.
+
+    Returns:
+        tuple: A tuple containing two elements:
+            - str: The modified text with placeholders instead of numbers.
+            - dict: A dictionary with keys "dates," "years," "prices," and "other" mapping to lists of
+              corresponding patterns found in the text.
+    """
 
     dates = re.findall(
         r"""(?:[\d]{1,2}[/-][\d]{1,2}[/-]
@@ -193,7 +252,27 @@ def removeNumbers(text):
         }
 
 
-def removeStopWords(text, lang):
+def removeStopWords(text: str, lang: str) -> Tuple[str, Dict[str, Dict[Counter]]]:
+    """
+    Removes stopwords from the input text based on the specified language
+
+    Args:
+        text (str): The input text
+        lang (str): The language code used to pick the stopwords list
+
+    Input:
+       text: "the sun shines"
+       lang: "en"
+
+    Output:
+        "sun shines"
+
+    Returns:
+        tuple: A tuple containing two elements:
+            - str: The modified text without stopwords.
+            - dict: A dictionary with the key "stopwords" mapping to a Counter
+            containing the retrieved stopwords and their counts.
+    """
 
     stopwords = make_stopwords(lang)
 
@@ -210,7 +289,24 @@ def removeStopWords(text, lang):
     return cleaned_text, {'stopwords': dict(counts)}
 
 
-def removePunctuation(text):
+def removePunctuation(text: str) -> Tuple[str, Dict[str, List[str]]]:
+    """
+    Removes punctuation from the input text
+
+    Args:
+        text (str)
+    
+    Input:
+       ""
+    Output:
+        ""
+
+    Returns:
+        tuple: A tuple containing two elements:
+            - str: The modified text without punctuation.
+            - dict: A dictionary with the key "punctuation" mapping to a list
+            of the punctuation signs extracted from the text.
+    """
     pattern = f'[{string.punctuation}¡¿]+'
 
     punctuation = re.findall(pattern, text)
